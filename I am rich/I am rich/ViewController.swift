aa2bde9cfd0a04a5518398918c8b4fa8ed9e8508
Yusufsimsekk <86342896+Yusufsimsekk@users.noreply.github.com>
2021-06-25 11:59:24 +0300

    Add files via upload

---
+++ I am rich/I am rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am rich
+//
+//  Created by Yusuf Şimşek on 23.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

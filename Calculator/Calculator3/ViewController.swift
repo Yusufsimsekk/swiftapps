0bc9387fb8a0d78a1e55bb3f55dfd2221c5b0989
Yusufsimsekk <86342896+Yusufsimsekk@users.noreply.github.com>
2021-06-27 20:46:22 +0300

    Calculator
    Calculator app without button functionality.
    Allignment between different devices and different orientations is perfect.
    There is no bug in alignment. 
    Calculator functionality will be added later.

---
+++ Calculator/Calculator3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator3
+//
+//  Created by Yusuf Şimşek on 27.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
